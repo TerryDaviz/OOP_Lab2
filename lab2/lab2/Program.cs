3e15261ae9e8d2c597cf6678f484abcf0dca956f
Ilya <iltihomsorok@gmail.com>
2022-09-19 10:19:19 +0300

    override object methods

--- lab2/lab2/Program.cs
+++ lab2/lab2/Program.cs
@@ -7,22 +7,66 @@ namespace lab2
     {
         private static void Main(string[] args)
         {
-            Stck s = new Stck(4);
-            Console.WriteLine(s.TopValue);
-            Stck s2 = new Stck(4, 5.0);
-            Console.WriteLine(s2[0]);
-            s2.RemoveFromStack();
-            s2.TopValue = 3.5;
-            s2.TopValue = 5.5;
+            Stck st = new Stck(4);
+            Console.WriteLine(st.TopValue);
+            Stck st2 = new Stck(4, 5.0);
+            Console.WriteLine(st2[0]);
+            st2.RemoveFromStack();
+            st2.AddTopValue(3.5);
+            st2.AddTopValue(5.5);
             double newValue = 5.6;
-            s2.ChangeTopValue(ref newValue);
+            st2.ChangeTopValue(ref newValue);
             string strValue = "4,4";
-            s2.AddStringValue(ref strValue);
-            s2.AddConvertedCharValue('a', out double bro);
-            s2.ShowStack();
+            st2.AddStringValue(ref strValue);
+            st2.AddConvertedCharValue('a', out double bro);
+            st2.ShowStack();
+            Stck st3 = new Stck();
+            st3.CreateStack(5);
+
+            Stck[] arr = new Stck[3] { st, st2, st3 };
+            Stck maxStck = new Stck(0.0F);
+            Stck minStck = new Stck(0.0F);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i].TopValue > maxStck.TopValue)
+                {
+                    maxStck = arr[i];
+                }
+            }
+            Console.WriteLine("\n=================");
+            Console.WriteLine("\nstack with max top value:");
+            maxStck.ShowStack();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i].TopValue < minStck.TopValue)
+                {
+                    minStck = arr[i];
+                }
+            }
+            Console.WriteLine("\n=================");
+            Console.WriteLine("\nstack with min top value: ");
+            minStck.ShowStack();
+            List<Stck> stacksWithNegatives = new List<Stck>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                foreach (double x in arr[i].WholeStack)
+                {
+                    if (x < 0)
+                    {
+                        stacksWithNegatives.Add(arr[i]);
+                    }
+                }
+            }
+            Console.WriteLine("\n=============");
+            Console.WriteLine("\nstacks with negative elements");
+            foreach (Stck x in stacksWithNegatives)
+            {
+                Console.WriteLine("\n\n");
+                x.ShowStack();
+            }
         }
 
-        internal class Stck
+        private partial class Stck
         {
             private const string container = "List";
             private static int objectCounter;
@@ -68,19 +112,18 @@ namespace lab2
                 this.id = random.Next(100) % 3 + id;
             }
 
-            public string getContainter => container;
-            public int getId => this.id;
+            public string Containter => container;
+            public int Id => this.id;
 
-            public int getObejectCounter => objectCounter;
+            public int ObejectCounter => objectCounter;
 
             public double TopValue
             {
                 get => this.stack[^1];
-                set
-                {
-                    double top = value;
-                    this.stack.Add(value);
-                }
+                set =>
+                    //double top = value;
+                    //this.stack.Add(value);
+                    this.stack[^1] = value;
             }
 
             public List<double> WholeStack => this.stack;
@@ -109,9 +152,42 @@ namespace lab2
                 this.stack.Add(Convert.ToDouble(strValue));
             }
 
+            public void AddTopValue(double value)
+            {
+                this.stack.Add(value);
+            }
+
             public void ChangeTopValue(ref double newValue)
             {
                 this.stack[^1] = newValue;
+            }
+
+            public List<double> CreateStack(int stackSize)
+            {
+                for (int i = 0; i < stackSize; i++)
+                {
+                    Random random = new Random();
+                    this.stack.Add(random.Next(-5, 5));
+                }
+                return this.stack;
+            }
+
+            public override bool Equals(object obj)
+            {
+                if (obj == null || !GetType().Equals(obj.GetType()))
+                {
+                    return false;
+                }
+                else
+                {
+                    Stck s = (Stck)obj;
+                    return this.stack == s.stack;
+                }
+            }
+
+            public override int GetHashCode()
+            {
+                return Convert.ToInt32(this.stack[0]) << 2 ^ 3 * objectCounter;
             }
 
             public List<double> RemoveFromStack()
@@ -127,6 +203,12 @@ namespace lab2
                 {
                     Console.Write("\n" + f);
                 }
+            }
+
+            public override string ToString()
+            {
+                double top = this.stack[this.stack.Count - 1];
+                return String.Format("top of stack:{0} ", top);
             }
         }
     }
